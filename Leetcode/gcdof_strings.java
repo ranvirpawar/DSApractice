42149f6f88652bcc713e5f8d418f222624a494ec
Ranvir <tylerturdenrgp@gmail.com>
2023-06-26 19:46:03 +0530

    leet75

---
+++ Leetcode/gcdof_strings.java
@@ -0,0 +1,18 @@
+1071. Greatest Common Divisor of Strings
+For two strings s and t, we say "t divides s" if and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
+
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+ 
+public String gcdOfStrings(String str1, String str2){
+    if(str2.length() > str1.length()){
+        return gcdOfStrings(str2, str1);
+    }
+    if(str2.equals(str1)){
+        return str1;
+    }
+    if (str1.startsWith(str2)){
+        return gcdOfStrings(str1.substring(str2.length()), str2);
+    }
+    return "";
+}
