8b1ffe30d1d16959366d859755e3a4ebdb10877e
Ranvir <tylerturdenrgp@gmail.com>
2023-08-22 19:31:11 +0530

    countPrimes better approach

--- number_theory/countPrimes.java
+++ number_theory/countPrimes.java
@@ -1,4 +1,7 @@
+
 package number_theory;
+
+import java.util.Arrays;
 
 public class countPrimes {
     public static boolean isPrime(int n) {
@@ -25,9 +28,32 @@ public class countPrimes {
 
     }
 
+    public static int countPrimeBetterapproach(int n) {
+        boolean[] barray = new boolean[n + 1];
+        Arrays.fill(barray, true);
+        barray[0] = false;
+        barray[1] = false;
+        for (int i = 0; i * i < n; i++) {
+            for (int j = i*i;  j <n; j += i) {
+                barray[j] = false;
+
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (barray[i] == true) {
+                count++;
+            }
+        }
+        return count;
+
+    }
+
     public static void main(String[] args) {
         int n = 10;
         // System.out.println(countPrimes(n));
+        int ans = countPrimeBetterapproach(n);
+        System.out.println(ans);
 
     }
 }
