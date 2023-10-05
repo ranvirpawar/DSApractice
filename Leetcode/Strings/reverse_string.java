4cee452337344bccec5e3251f9835ebf560770be
Ranvir <tylerturdenrgp@gmail.com>
2023-10-05 23:53:10 +0530

    reverse words of string / optimize further

--- Leetcode/Strings/reverse_string.java
+++ Leetcode/Strings/reverse_string.java
@@ -7,6 +7,9 @@
 
 // Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
 
+// Input: s = "the sky is blue"
+// Output: "blue is sky the"
+
 import strings.strings;
 
 class Solution {
