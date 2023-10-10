1f61445012bc08b66078579cf00b9694a94dc32d
Ranvir <tylerturdenrgp@gmail.com>
2023-10-10 22:55:20 +0530

    loops2Leetcode

---
+++ Hackerrank/java/loops2.java
@@ -0,0 +1,31 @@
+// Sample Input
+
+// 2
+// 0 2 10
+// 5 3 5
+// Sample Output
+
+// 2 6 14 30 62 126 254 510 1022 2046
+// 8 14 26 50 98
+
+class Solution {
+    public static void main(String[] argh) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        int a, b, n;
+        for (int i = 0; i < t; i++) {
+            a = in.nextInt();
+            b = in.nextInt();
+            n = in.nextInt();
+            int series = a;
+            for (int j = 0; j < n; j++) {
+                series += (Math.pow(2, j) * b);
+                System.out.print(series + " ");
+            }
+            System.out.println();
+        }
+
+        in.close();
+
+    }
+}
